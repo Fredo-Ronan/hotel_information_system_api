81244a6f042dcfa2bdd9129ccc3e1c73971e30ae
Fredo-Ronan <fredocode06@gmail.com>
2023-12-14 19:25:23 +0700

    some styling

--- resources/views/verifyOk.blade.php
+++ resources/views/verifyOk.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,13 +12,32 @@
             justify-content: center;
             align-items: center;
             height: 100%;
+            font-family: sans-serif;
         }
     </style>
 </head>
+
 <body>
     <div class="container">
         <h2>Selamat! Email anda terverifikasi dan Akun anda sudah aktif!</h2>
+        <br>
         <h4>Anda akan diarahkan ke halaman login dalam {{ $delay }} detik...</h4>
+        <br><br>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
+            <circle fill="#19D600" stroke="#19D600" stroke-width="15" r="15" cx="40" cy="65">
+                <animate attributeName="cy" calcMode="spline" dur="2" values="65;135;65;"
+                    keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.4"></animate>
+            </circle>
+            <circle fill="#19D600" stroke="#19D600" stroke-width="15" r="15" cx="100" cy="65">
+                <animate attributeName="cy" calcMode="spline" dur="2" values="65;135;65;"
+                    keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="-.2"></animate>
+            </circle>
+            <circle fill="#19D600" stroke="#19D600" stroke-width="15" r="15" cx="160" cy="65">
+                <animate attributeName="cy" calcMode="spline" dur="2" values="65;135;65;"
+                    keySplines=".5 0 .5 1;.5 0 .5 1" repeatCount="indefinite" begin="0"></animate>
+            </circle>
+        </svg>
     </div>
 </body>
+
 </html>
