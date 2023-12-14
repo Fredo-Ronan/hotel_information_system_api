109862241e34db114b4bed8a406a210fce97d82b
Fredo-Ronan <fredocode06@gmail.com>
2023-12-14 11:04:24 +0700

    fix bug passport tokens [maybe]

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         config(['app.locale', 'id']);
-        Carbon::setlocale('id');
+        Carbon::setLocale('id');
         date_default_timezone_set('Asia/Jakarta');
     }
 }
