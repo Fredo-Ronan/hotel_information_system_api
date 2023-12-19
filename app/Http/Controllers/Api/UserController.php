57dcef46ac2f2456e37e91489114db6f8cc468f1
Fredo-Ronan <fredocode06@gmail.com>
2023-12-19 22:21:35 +0700

    change some format

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -41,7 +41,7 @@ class UserController extends Controller
                     'email'=> $user->email,
                     'no_telp'=> $user->no_telp,
                     'username'=> $user->username,
-                    'profile_img' => base64_encode($user->profile_img),
+                    'profile_img' => "data:image/" . $user->img_ext . ";base64," . base64_encode($user->profile_img),
                     'verify_key' => $user->verify_key,
                     'active' => $user->active,
                     'created_at' => $user->created_at,
