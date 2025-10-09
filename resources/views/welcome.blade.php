88a1f7e10b65559a20076c30fbd761c2f1451720
Fredo-Ronan <fredocode06@gmail.com>
2025-10-09 20:24:34 +0700

    update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,8 +11,12 @@
         <h2>SELAMAT DATANG DI API TUGAS BESAR WEB</h2>
         <p>Website ini hanya sebagai API untuk frontend React Tugas Besar Matakuliah Pemrograman Web</p>
         <p>
-            Berikut adalah link frontend nya
+            Frontend dapat di akses di
             <a href="https://the5stars-hotel.vercel.app/">https://the5stars-hotel.vercel.app/</a>
+        </p>
+        <p>
+            Dokumentasi route api bisa di lihat 
+            <a href="https://github.com/Fredo-Ronan/hotel_information_system_api/blob/main/README.md">disini</a>
         </p>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
