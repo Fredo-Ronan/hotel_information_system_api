78e5da0474a2d39761dc6b4d15cb045705cf88c4
Fredo-Ronan <fredocode06@gmail.com>
2025-10-12 15:55:14 +0700

    update docker compose and build scripts

--- migrate.sh
+++ migrate.sh
@@ -1,2 +1,7 @@
 # Run migrations
 sudo docker compose exec app php artisan migrate
+
+echo ""
+echo ""
+echo "Migrations completed!"
+echo "Now run 'passport_config.sh' to set up Laravel Passport."
--- passport_config.sh
+++ passport_config.sh
@@ -1,1 +1,6 @@
 sudo docker compose exec app php artisan passport:install
+
+echo ""
+echo ""
+echo "Laravel Passport setup completed!"
+echo "visit or curl http://localhost:8080 to check the API."
